bf1491a848facbc4c0ffe7de6faa98a741f2dfcd
leandro-tecnico <leandrorcarvalho@prof.educacao.sp.gov.br>
2025-04-30 20:06:36 -0300

    Criação do arquivo app_sec.py que implementa a camada de segurança jwt

--- app.py
+++ app.py
@@ -4,24 +4,21 @@ app = Flask(__name__)
 
 # Dados iniciais (simulação de banco de dados)
 produtos = [
-
     {"id": 1, "nome": "Camiseta", "preco": 50.00},
-
-    {"id": 2, "nome": "Tênis", "preco": 120.00}
-
-    ]
+    {"id": 2, "nome": "Tênis", "preco": 120.00},
+]
 
 
 # Rota para listar todos os produtos (GET)
-@app.route('/produtos', methods=['GET'])
-
+@app.route("/produtos", methods=["GET"])
 def listar_produtos():
     return jsonify(produtos)
 
+
 # Rota para adicionar um novo produto (POST)
 
-@app.route('/produtos', methods=['POST'])
 
+@app.route("/produtos", methods=["POST"])
 def adicionar_produto():
 
     novo_produto = request.get_json()
@@ -32,15 +29,13 @@ def adicionar_produto():
 
 
 # Rota para atualizar um produto existente (PUT)
-@app.route('/produtos/<int:id>', methods=['PUT'])
-
+@app.route("/produtos/<int:id>", methods=["PUT"])
 def atualizar_produto(id):
 
-    produto = next((p for p in produtos if p['id'] == id), None)
+    produto = next((p for p in produtos if p["id"] == id), None)
     if produto is None:
 
         return jsonify({"erro": "Produto não encontrado!"}), 404
-
 
     dados_atualizados = request.get_json()
 
@@ -48,17 +43,16 @@ def atualizar_produto(id):
 
     return jsonify({"mensagem": "Produto atualizado com sucesso!"})
 
-# Rota para remover um produto (DELETE)
-@app.route('/produtos/<int:id>', methods=['DELETE'])
 
+# Rota para remover um produto (DELETE)
+@app.route("/produtos/<int:id>", methods=["DELETE"])
 def remover_produto(id):
 
-    produto = next((p for p in produtos if p['id'] == id), None)
+    produto = next((p for p in produtos if p["id"] == id), None)
 
     if produto is None:
 
         return jsonify({"erro": "Produto não encontrado!"}), 404
-
 
     produtos.remove(produto)
 
@@ -66,5 +60,5 @@ def remover_produto(id):
 
 
 # Executa a aplicação
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
